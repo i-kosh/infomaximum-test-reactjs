c630f91c22e492fd6fcba5bbae8c30e7484cf12b
Igor Koshelev <igor.koshelev440@gmail.com>
2020-08-22 01:12:36 +0300

    fix: revert

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,16 @@
-import Button from "./Button";
+import React, { FunctionComponent } from "react";
+import "./style.scss";
+
+export interface Props {
+  disabled?: boolean;
+}
+
+const Button: FunctionComponent<Props> = (props) => {
+  return (
+    <button disabled={props.disabled} className="button">
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
