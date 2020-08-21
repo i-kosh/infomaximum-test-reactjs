755ef5a25dd7947f761699b084a0a35880edb9fa
Igor Koshelev <igor.koshelev440@gmail.com>
2020-08-22 01:12:44 +0300

    feat: revert

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,40 @@
-import Input from "./Input";
+import React, { Component } from "react";
+import classNames from "classnames";
+import "./style.scss";
 
-export default Input;
+export interface Props {
+  err?: boolean;
+  disabled?: boolean;
+}
+
+export interface State {
+  value: any;
+}
+
+export default class Input extends Component<Props, State> {
+  state: State = {
+    value: undefined,
+  };
+
+  handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({
+      value: e.target.value,
+    });
+  }
+
+  render() {
+    const rootClasses = classNames({
+      input: true,
+      "input--error": this.props.err,
+    });
+
+    return (
+      <input
+        className={rootClasses}
+        disabled={this.props.disabled}
+        value={this.state.value}
+        onChange={this.handleChange.bind(this)}
+      />
+    );
+  }
+}
