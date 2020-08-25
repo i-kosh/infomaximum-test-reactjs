838e898b24676b43535471ecd428c5cf76e8c0c1
Igor Koshelev <igor.koshelev440@gmail.com>
2020-08-25 18:40:46 +0300

    feat: WIP

--- src/components/pages/ProcessList/index.tsx
+++ src/components/pages/ProcessList/index.tsx
@@ -1,12 +1,32 @@
 import React, { FunctionComponent } from "react";
+import ProcessItem from "../../ProcessItem";
+import "./style.scss";
 
 interface Props {}
 
 const ProcessList: FunctionComponent<Props> = (props) => {
   return (
     <section className="process-list">
-      <h2 className="process-list__heading">test</h2>
-      <div className="process-list__content"></div>
+      <h2 className="process-list__heading">Список процессов</h2>
+      <div className="process-list__content">
+        <ul className="process-list__list">
+          <li className="process-list__item">
+            <ProcessItem />
+          </li>
+          <li className="process-list__item">
+            <ProcessItem />
+          </li>
+          <li className="process-list__item">
+            <ProcessItem />
+          </li>
+          <li className="process-list__item">
+            <ProcessItem />
+          </li>
+          <li className="process-list__item">
+            <ProcessItem />
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
