8d250065354a31debfa97d37ef19da369767866d
Igor Koshelev <igor.koshelev440@gmail.com>
2020-08-25 18:39:10 +0300

    feat: add component

---
+++ src/components/ProfileEdit/index.tsx
@@ -0,0 +1,45 @@
+import React, { FunctionComponent } from "react";
+import InputWE from "../InputErrorMessage";
+import InputPassword from "../InputPassword";
+import "./style.scss";
+
+const ProfileEdit: FunctionComponent = (props) => {
+  return (
+    <div className="profile-edit">
+      <form>
+        <label className="profile-edit__label">
+          <span className="profile-edit__label-text">Имя</span>
+          <div className="profile-edit__input">
+            <InputWE placeholder="Не задано" />
+          </div>
+        </label>
+        <label className="profile-edit__label">
+          <span className="profile-edit__label-text">Фамилия</span>
+          <div className="profile-edit__input">
+            <InputWE placeholder="Не задано" />
+          </div>
+        </label>
+        <label className="profile-edit__label">
+          <span className="profile-edit__label-text">Электронная почта</span>
+          <div className="profile-edit__input">
+            <InputWE type="email" placeholder="Не задано" />
+          </div>
+        </label>
+        <label className="profile-edit__label">
+          <span className="profile-edit__label-text">Новый пароль</span>
+          <div className="profile-edit__input">
+            <InputPassword placeholder="Не задано" />
+          </div>
+        </label>
+        <label className="profile-edit__label">
+          <span className="profile-edit__label-text">Повторите пароль</span>
+          <div className="profile-edit__input">
+            <InputPassword placeholder="Не задано" />
+          </div>
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default ProfileEdit;
