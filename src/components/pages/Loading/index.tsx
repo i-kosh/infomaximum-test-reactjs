7b9bbb3aee44a6973777922ca2a31f4f4ae9f27a
Igor Koshelev <igor.koshelev440@gmail.com>
2020-08-27 11:52:56 +0300

    feat: add loading page

---
+++ src/components/pages/Loading/index.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from "react";
+import classnames from "classnames";
+import "./style.scss";
+
+interface Props {
+  hidden: boolean;
+}
+
+const Profile: FunctionComponent<Props> = (props) => {
+  const rootClasses = classnames({
+    "loading-screen": true,
+    "loading-screen--hidden": props.hidden,
+  });
+
+  return <section className={rootClasses}>...</section>;
+};
+
+export default Profile;
