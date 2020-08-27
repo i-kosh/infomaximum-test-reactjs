b3bf36038cb850f04b5548e8f2960639a5febffd
Igor Koshelev <igor.koshelev440@gmail.com>
2020-08-27 15:59:16 +0300

    feat: add argument

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -12,12 +12,15 @@ export function emailValidate(email: string | undefined): string | null {
   return error;
 }
 
-export function passwordValidate(password: string | undefined): string | null {
+export function passwordValidate(
+  password: string | undefined,
+  required = true
+): string | null {
   let error: string | null = null;
 
-  if (!password) {
+  if (!password && required) {
     error = "Введите пароль";
-  } else if (password.length < 8) {
+  } else if (password && password.length < 8) {
     error = "минимум 8 символов";
   }
 
