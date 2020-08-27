b498b94351a8f4eb8ed15e8ba380a793c65c8a00
Igor Koshelev <igor.koshelev440@gmail.com>
2020-08-27 11:53:29 +0300

    feat: add user name and logout button

--- src/components/pages/Profile/index.tsx
+++ src/components/pages/Profile/index.tsx
@@ -1,4 +1,6 @@
 import React, { FunctionComponent } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser, logOut } from "../../../store/userSlice";
 import Button from "../../Button";
 import ProfileForm from "../../ProfileEdit";
 import "./style.scss";
@@ -6,10 +8,18 @@ import "./style.scss";
 interface Props {}
 
 const Profile: FunctionComponent<Props> = (props) => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const userProfile = user.profile;
+
+  const fullUserName = userProfile
+    ? `${userProfile.firstName} ${userProfile.secondName}. `
+    : "";
+
   return (
     <section className="profile">
       <div className="profile__top">
-        <h2 className="profile__heading">Борис Годунов. Редактирование</h2>
+        <h2 className="profile__heading">{fullUserName}Редактирование</h2>
         <div className="profile__save-btn">
           <Button>Сохранить</Button>
         </div>
@@ -17,6 +27,15 @@ const Profile: FunctionComponent<Props> = (props) => {
       <div className="profile__content">
         <ProfileForm />
       </div>
+      <Button
+        onClick={() => {
+          dispatch(logOut());
+        }}
+        type="button"
+        style={{ width: "fit-content", marginTop: "24px" }}
+      >
+        Выйти
+      </Button>
     </section>
   );
 };
