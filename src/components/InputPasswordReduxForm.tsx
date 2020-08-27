b47ddded3fc128056c71f6682b008772f9c97bb4
Igor Koshelev <igor.koshelev440@gmail.com>
2020-08-27 15:59:40 +0300

    feat: add prop

--- src/components/InputPasswordReduxForm.tsx
+++ src/components/InputPasswordReduxForm.tsx
@@ -2,16 +2,18 @@ import React, { FunctionComponent } from "react";
 import InputPassword, { Props as InputPorops } from "./InputPassword";
 import { WrappedFieldProps } from "redux-form";
 
-export interface Props extends InputPorops, WrappedFieldProps {}
+export interface Props extends InputPorops, WrappedFieldProps {
+  shouldAlwaysValidate?: boolean;
+}
 
 const InputReduxForm: FunctionComponent<Props> = (props) => {
-  const { input, meta, ...restProps } = props;
+  const { input, meta, shouldAlwaysValidate, ...restProps } = props;
 
   return (
     <InputPassword
       {...restProps}
       {...input}
-      err={meta.submitFailed && meta.error}
+      err={shouldAlwaysValidate ? meta.error : meta.touched && meta.error}
     />
   );
 };
--- src/components/InputReduxForm.tsx
+++ src/components/InputReduxForm.tsx
@@ -2,13 +2,19 @@ import React, { FunctionComponent } from "react";
 import InputWE, { Props as InputPorops } from "./InputErrorMessage";
 import { WrappedFieldProps } from "redux-form";
 
-export interface Props extends InputPorops, WrappedFieldProps {}
+export interface Props extends InputPorops, WrappedFieldProps {
+  shouldAlwaysValidate?: boolean;
+}
 
 const InputReduxForm: FunctionComponent<Props> = (props) => {
-  const { input, meta, ...restProps } = props;
+  const { input, meta, shouldAlwaysValidate, ...restProps } = props;
 
   return (
-    <InputWE {...restProps} {...input} err={meta.submitFailed && meta.error} />
+    <InputWE
+      {...restProps}
+      {...input}
+      err={shouldAlwaysValidate ? meta.error : meta.touched && meta.error}
+    />
   );
 };
 
